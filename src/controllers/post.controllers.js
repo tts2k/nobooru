674b02b81f2f4631e4987c497c4a915d705c5805
tts2k <tranthaison2000@gmail.com>
2022-06-06 15:15:17 -0400

    server: Move db queries out of controllers

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -1,50 +1,16 @@
 const { resolve } = require("path");
-const prisma = require("../prisma");
+const { Prisma } = require('@prisma/client');
 const { toPositiveInteger } = require("../utils/misc.util");
-const { generateSha256Sum, processImage, writeBufferToPath } = require("../utils/file.util");
 const AppError = require("../error/app-error");
-const { Prisma } = require("@prisma/client");
+const postRepo = require("../repositories/post.repo");
+const { generateSha256Sum, processImage, writeBufferToPath } = require("../utils/file.util");
 
 /* GET */
 const getPostsLatest = async (req, res, next) => {
     try {
         const page = req.body.page ? req.body.page : 0;
-
-        const result = await prisma.$transaction([
-            prisma.post.count(),
-            prisma.post.findMany({
-                orderBy: { createdAt: "asc" },
-                take: 20,
-                skip: 20 * page,
-                select: {
-                    id: true,
-                    fileDir: true,
-                    checksum: true,
-                    type: {
-                        select: {
-                            id: true,
-                            name: true
-                        }
-                    },
-                    postTags: {
-                        select: {
-                            tag:  {
-                                select: {
-                                    id: true,
-                                    name: true
-                                }
-                            }
-                        }
-                    }
-                }
-            })
-        ]);
-
-        res.status(200).json({
-            posts: result[1],
-            page: page + 1,
-            totalPages: Math.ceil(result[0] / 20)
-        });
+        const result = await postRepo.getLatest(page);
+        return res.status(200).json(result);
     } catch (err) {
         next(AppError.internalError(err.message))
     }
@@ -58,30 +24,7 @@ const getPostById = async (req, res, next) => {
             return next(AppError.badRequest("Invalid request."));
         }
 
-        // Get post
-        const post = await prisma.post.findUnique({
-            where: {
-                id: id
-            },
-            include: {
-                type: {
-                    select: {
-                        id: true,
-                        name: true
-                    }
-                },
-                postTags: {
-                    select: {
-                        tag:  {
-                            select: {
-                                id: true,
-                                name: true
-                            }
-                        }
-                    }
-                }
-            }
-        });
+        const post = await postRepo.getById(id);
 
         if (post === null) {
             return next(AppError.notFound("Post not found."));
@@ -109,55 +52,10 @@ const getPostsByTags = async (req, res, next) => {
             return next(AppError.badRequest("Tag cannot be empty."));
         }
 
-        // Creating where clause
-        const where = { AND: []};
-        tags.forEach(e => {
-            where.AND.push({
-                postTags: {
-                    some: {
-                        tag: { name: e }
-                    }
-                }
-            })
-        })
+        // Get posts
+        const result = await postRepo.getByTags(tags, page);
 
-        // Get post
-        const result = await prisma.$transaction([
-            prisma.post.count({ where: where }),
-            prisma.post.findMany({
-                orderBy: { createdAt: "asc" },
-                where: where,
-                take: 20,
-                skip: 20 * page,
-                select: {
-                    id: true,
-                    fileDir: true,
-                    checksum: true,
-                    type: {
-                        select: {
-                            id: true,
-                            name: true
-                        }
-                    },
-                    postTags: {
-                        select: {
-                            tag:  {
-                                select: {
-                                    id: true,
-                                    name: true,
-                                }
-                            }
-                        }
-                    }
-                }
-            })
-        ]);
-
-        return res.status(200).json({
-            posts: result[1],
-            page: page + 1,
-            totalPages: result[0]
-        });
+        return res.status(200).json(result);
     } catch (err) {
         next(AppError.internalError(err.message));
     }
@@ -176,47 +74,23 @@ const createPostFromFile = async (req, res, next) => {
         const imageFile = files.image[0];
         const jsonFile = files.json[0];
 
-        // Process image (check image type, generate thumbnail)
+        // Process image (get image metadata, generate thumbnail)
         const imageDetails = await processImage(imageFile.buffer);
 
         // Get image checksum
         const checksum = generateSha256Sum(imageFile.buffer);
 
-
         // Create new post record
         const postDetails = JSON.parse(jsonFile.buffer.toString()); // Parse json for post metadata
-        const postTagsConnect = postDetails.tags.map(e => {
-            return {
-                tag: {
-                    connect: {
-                        id: e
-                    }
-                }
-            }
-        });
 
-        const post = await prisma.post.create({
-            data: {
-                originalName: imageFile.originalname,
-                fileDir: imageDir,
-                checksum: checksum,
-                fileSize: imageFile.size,
-                format: imageFile.mimetype,
-                description: postDetails.description,
-                width: imageDetails.width,
-                height: imageDetails.height,
-                type: { connect: { id: postDetails.type } },
-                user: { connect: { id: req.userId } },
-                postTags: { create: postTagsConnect }
-            }
-        });
+        await postRepo.createPost(imageFile, imageDir, checksum, imageDetails, postDetails, req.userId);
 
         // Move image to configured directory
         const fileName = `${checksum}.${imageDetails.format}`;
         const moveRaw = writeBufferToPath(imageFile.buffer, `${imageDir}/raws/`, fileName);
         const moveThumb = writeBufferToPath(imageDetails.thumbnailBuffer, `${imageDir}/thumbnails/`, `thumb-${fileName}`);
 
-        await Promise.all([moveRaw, moveThumb]);
+        const post = await Promise.all([moveRaw, moveThumb]);
 
         res.status(200).send({
             status: "success",
@@ -237,16 +111,7 @@ const deletePostById = async (req, res, next) =>{
         if (!req.body.postId) {
             next(AppError.badRequest("Post id is empty"));
         }
-
-        const deletePostTags = prisma.postTag.deleteMany({
-            where: { postId: req.body.postId }
-        });
-        const deletePost = prisma.post.delete({
-            where: { id: req.body.postId }
-        });
-
-        await prisma.$transaction([deletePostTags, deletePost]);
-
+        await postRepo.deleteById(req.body.postId);
         return res.status(200).json({ message: "Post deleted" });
     }
     catch (err) {
---
+++ src/repositories/post.repo.js
@@ -0,0 +1,217 @@
+const prisma = require("../prisma");
+const { Prisma } = require('@prisma/client');
+const _getMany = async (page, where) => {
+    const posts = await prisma.$transaction([
+        prisma.post.count(),
+        prisma.post.findMany({
+            orderBy: { createdAt: "asc" },
+            where: where,
+            take: 20,
+            skip: 20 * page,
+            select: {
+                id: true,
+                fileDir: true,
+                checksum: true,
+                type: {
+                    select: {
+                        name: true
+                    }
+                },
+                postTags: {
+                    select: {
+                        tag:  {
+                            select: {
+                                name: true,
+                                namespace: { select: { name: true } }
+                            }
+                        }
+                    }
+                }
+            }
+        })
+    ]);
+
+    const result = {
+        page: page + 1,
+        totalPages: Math.ceil(posts[0] / 20)
+    };
+    result.posts = posts[1].map(e => {
+        return {
+            id: e.id,
+            checksum: e.checksum,
+            fileDir: e.fileDir,
+            type: e.type.name,
+            tags: e.postTags.map(e => {
+                return {
+                    name: e.tag.name,
+                    namespace: (e.tag.namespace)
+                        ? e.tag.namespace.name
+                        : ""
+                }
+            })
+        }
+    });
+
+    return result;
+}
+
+const getLatest = async (page) => {
+    try {
+        return await _getMany(page);
+    } catch (err) {
+        console.error(err);
+        throw new Error("Internal server error");
+    }
+}
+
+const getByTags = async (tags, page) => {
+    // Create where clause
+    const where = { AND: []};
+    tags.forEach(e => {
+        where.AND.push({
+            postTags: {
+                some: {
+                    tag: { name: e }
+                }
+            }
+        })
+    })
+
+    return await _getMany(page, where);
+}
+
+const getById = async (id) => {
+    try {
+        // Get post
+        const post = await prisma.post.findUnique({
+            where: {
+                id: id
+            },
+            include: {
+                user:{
+                    select: {
+                        id: true,
+                        name: true
+                    }
+                },
+                type: {
+                    select: {
+                        id: true,
+                        name: true
+                    }
+                },
+                postTags: {
+                    select: {
+                        tag:  {
+                            select: {
+                                id: true,
+                                name: true,
+                                namespace: {
+                                    select: { name: true }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        console.log(post.postTags[0].tag.namespace.name);
+
+        if (post === null)
+            return post;
+
+        const result = {
+            id: post.id,
+            checksum: post.checksum,
+            originalName: post.originalName,
+            fileDir: post.fileDir,
+            width: post.width,
+            height: post.height,
+            fileSize: post.fileSize,
+            format: post.format,
+            description: post.description,
+            type: post.type.name,
+            tags: post.postTags.map(e => {
+                return {
+                    name: e.tag.name,
+                    namespace: (e.tag.namespace)
+                        ? e.tag.namespace.name
+                        : ""
+                }
+            }),
+            uploader: {
+                id: post.user.id,
+                name: post.user.name
+            },
+            uploadDate: post.createdAt
+        }
+
+        return result;
+    } catch (err) {
+        console.error(err);
+        throw new Error("Internal server error");
+    }
+}
+
+const deleteById = async (id) => {
+    try {
+        const deletePostTags = prisma.postTag.deleteMany({
+            where: { postId: id }
+        });
+        const deletePost = prisma.post.delete({
+            where: { id: id }
+        });
+
+        await prisma.$transaction([deletePostTags, deletePost]);
+    } catch(err) {
+        console.error(err);
+        throw new Error("Internal server error");
+    }
+}
+
+const createPost = async (imageFile, imageDir, checksum, imageDetails, postDetails, userId) => {
+    const postTagsConnect = postDetails.tags.map(e => {
+        return {
+            tag: {
+                connect: {
+                    id: e
+                }
+            }
+        }
+    });
+    try {
+        const post = await prisma.post.create({
+            data: {
+                originalName: imageFile.originalname,
+                fileDir: imageDir,
+                checksum: checksum,
+                fileSize: imageFile.size,
+                format: imageFile.mimetype,
+                description: postDetails.description,
+                width: imageDetails.width,
+                height: imageDetails.height,
+                type: { connect: { id: postDetails.type } },
+                user: { connect: { id: userId } },
+                postTags: { create: postTagsConnect }
+            }
+        });
+
+        return post;
+    }
+    catch(err) {
+        console.error(err);
+        if (err instanceof Prisma.PrismaClientKnownRequestError) {
+            throw err;
+        }
+        throw new Error("Internal server error");
+    }
+}
+
+module.exports = {
+    getLatest,
+    getById,
+    getByTags,
+    deleteById,
+    createPost
+}

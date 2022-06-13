7ecab528c903fee5e04f02efd1bba113b0af42b9
tts2k <tranthaison2000@gmail.com>
2022-06-13 18:52:10 -0400

    server: clean up auth controller

--- server/src/controllers/auth.controllers.js
+++ server/src/controllers/auth.controllers.js
@@ -6,15 +6,20 @@ const userRepo = require("../repositories/user.repo");
 const { isStringNotNullOrEmpty } = require("../utils/misc.util");
 const HttpResCode = require("../constants").HttpResCode;
 
+const checkInput = (req) => {
+    const username = req.body?.username;
+    const password = req.body?.password;
+    //Better input checking will be implemented later
+    if (!isStringNotNullOrEmpty(username) || !isStringNotNullOrEmpty(password)) {
+        return next(AppError.badRequest("Username or password cannot be empty"));
+    }
+
+    return { username, password };
+}
+
 const signUp = async (req, res, next) => {
     try {
-        const username = req.body?.username;
-        const password = req.body?.password;
-        //Better input checking will be implemented later
-        if (!isStringNotNullOrEmpty(username) || !isStringNotNullOrEmpty(password)) {
-            return next(AppError.badRequest("Username or password cannot be empty"));
-        }
-
+        const { username, password } = checkInput(req);
         await userRepo.createUser(username, password);
         return res.status(HttpResCode.Created).send({ message: "User was registered successfully"});
     }
@@ -29,15 +34,13 @@ const signUp = async (req, res, next) => {
 
 const signIn = async (req, res, next) => {
     try {
-        if (!req.body?.username || req.body?.username.length === 0) {
-            return next(AppError.badRequest("Username cannot be empty."));
-        }
+        const { username, password } = checkInput(req);
 
-        const user = await userRepo.getUserByName();
+        const user = await userRepo.getUserByName(username);
 
         let passwordCorrect =
             user
-            ? bcrypt.compareSync(req.body.password, user.passwordHash)
+            ? bcrypt.compareSync(password, user.passwordHash)
             : false
 
         if (!passwordCorrect) {
